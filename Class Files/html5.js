e3fd28fc1d077065b0b78980b3e2ecb5224a8a81
10deken <10zind@gmail.com>
2013-11-01 15:40:55 -0500

    Folder changes (moved stuff around)

--- Class Files/html5.js
+++
@@ -1,7 +0,0 @@
-// HTML5 for Internet Explorer versions < 9
-var e = ("abbr,article,aside,audio,canvas,datalist,details," +
-    "figure,footer,header,hgroup,mark,menu,meter,nav,output," +
-    "progress,section,time,video").split(',');
-  for (var i = 0; i < e.length; i++) {
-    document.createElement(e[i]);
-  }
